92f9bc9152882330adce578e50e74d4f27517211
unknown <dsml@live.com>
2024-06-22 12:41:24 -0600

    corsOptions

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 import productsRoutes from './routes/products.js'
 
 const app = express()
 const PORT = 5000
+
+const corsOptions = {
+    origin: [   
+                'http://localhost:3000/productos', 
+                'http://localhost:3001/productos', 
+                'http://localhost:3002/productos', 
+                'http://localhost:3004/productos', 
+                'http://localhost:3005/productos', 
+                'https://empire-sm.vercel.app/productos'
+            ]
+}
+
+app.use(cors(corsOptions))
 
 app.use(bodyParser.json())
 
